951dd63e98b2d816254ad2bc258b5728748381a8
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-06-24 19:06:20 -0300

    Components and props

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
+  )
+}
