3c6f075da118803e319bf7179d9137dac1072731
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-07-13 18:04:04 -0300

    Working with data

--- primeiro-projeto-node/src/routes/appointments.routes.ts
+++ primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -5,6 +5,11 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+    return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
     const parsedDate = startOfHour(parseISO(date));
@@ -16,7 +21,11 @@ appointmentsRouter.post('/', (request, response) => {
         return response.status(400).json({ message: "This appointment is already booked" });
     }
     
-    const appointment = appointmentsRepository.create(provider, parsedDate);
+    const appointment = appointmentsRepository.create({
+        provider,
+        date: parsedDate,
+    });
+    
     return response.json(appointment);
 });
 
