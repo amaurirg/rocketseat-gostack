5be460d6dac948aabf1932a354aa1ce7864c5aeb
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-07-17 01:53:10 -0300

    Relating the code to the database

--- primeiro-projeto-node/src/database/migrations/1594931787307-CreateAppointments.ts
+++ primeiro-projeto-node/src/database/migrations/1594931787307-CreateAppointments.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateAppointments1594931787307 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments');
     }
 
 }
