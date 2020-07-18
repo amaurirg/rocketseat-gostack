bc370f3e6f1abf43399afbec2541d31e2b0fe958
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2020-07-18 18:51:58 -0300

    Created Users models and migrations

--- primeiro-projeto-node/src/database/migrations/1595102323419-CreateUsers.ts
+++ primeiro-projeto-node/src/database/migrations/1595102323419-CreateUsers.ts
@@ -1,12 +1,12 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAppointments1594931787307 implements MigrationInterface {
+export default class CreateUsers1595102323419 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -16,14 +16,17 @@ export class CreateAppointments1594931787307 implements MigrationInterface {
                         default: 'uuid_generate_v4()'
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
-                        isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false,
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -41,7 +44,7 @@ export class CreateAppointments1594931787307 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 
 }
