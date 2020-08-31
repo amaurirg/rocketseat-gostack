902d38b9f92894e9f62e1abfeb2aecae078a9c2d
Amauri <amaurirg@terra.com.br>
2020-08-31 18:24:33 -0300

    Start connect to API

--- gobarber-web/backend/src/server.ts
+++ gobarber-web/backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors'
 import routes from './routes';
 import './database';
@@ -7,6 +8,7 @@ import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
